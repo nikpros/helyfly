ea33b57ca69e4b3b2f0e762c9d5beb436db4b3d5
Ivan K <ivan_kalita90@mail.ru>
2019-09-22 21:35:55 +0300

    comleted Comments-block

--- src/js/common.js
+++ src/js/common.js
@@ -1,6 +1,7 @@
 jQuery(document).ready(function($) {
     let buttonShowmore = $('.comments__show-more');
     let hideComments = $('.comments__item').filter('.hide');
+    let hideRows = $('.comments > .row').filter('.hide');
     let extraString = '<img class="mr-2" src="/assets/images/arrow.svg"></img>'
     let newString = $(buttonShowmore).text() + ` (${hideComments.length}) `;
 
@@ -8,6 +9,7 @@ jQuery(document).ready(function($) {
 
     $(buttonShowmore).on('click', function(e) {
         $(hideComments).removeClass('hide');
+        $(hideRows).removeClass('hide');
         $(this).fadeOut()
     })
 })
